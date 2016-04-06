6a362b76fa29a9cf30498d9ae99700af35c5e10c
Fabricio Paes <fabriciopaes1@gmail.com>
2016-04-06 00:43:13 -0300

    sql

--- sqlCode/inventario_benfeitorias.sql
+++ sqlCode/inventario_benfeitorias.sql
@@ -5,7 +5,4 @@ INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Cercas Convenci
 INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Depósito de Ração", 	"Ud", 1.0, 	1000, 	10, 13);
 INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Cochos", 				"Ud", 1.0, 	300, 	10, 13);
 INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Açude", 				"Ud", 1.0, 	2500, 	20, 13);
-INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Casa", 				"Ud", 0.0, 	0.0, 	0, 	13);
-INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Bebedouro", 			"Ud", 0.0, 	0.0, 	0, 	13);
-INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Silo", 				"Ud", 0.0, 	0.0, 	0, 	13);
 INSERT INTO projeto_pesquisa.inventario_benfeitorias VALUES (0, "Armazém", 				"R$", 0.0, 	0.0, 	1, 	13);
