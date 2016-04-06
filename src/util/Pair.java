5d62d1207384218157611a694a45b526f1fab752
JeffersonSales <jefferson360@gmail.com>
2016-04-06 01:36:31 -0300

    Atualizado

---
+++ src/util/Pair.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Android SDK (adapted to plain Java)
+ */
+public class Pair<F, S> {
+    
+    public F first;
+    public S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+        Pair<?, ?> p = (Pair<?, ?>) o;
+        return p.first.equals(first) && p.second.equals(second);
+    }
+
+    @Override
+    public int hashCode() {
+        return (first == null ? 0 : first.hashCode()) ^ (second == null ? 0 : second.hashCode());
+    }
+
+    public static <A, B> Pair <A, B> create(A a, B b) {
+        return new Pair<>(a, b);
+    }
+}
